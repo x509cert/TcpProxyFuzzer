f483f11352d14b1d149f402b8db183400cfb6c37
Michael Howard <mikehow@microsoft.com>
2023-11-16 17:20:05 -0800

    Huge changes. Added naughty word file, timstamp, better tracing, overlong UTF8

--- TcpProxyFuzzer/Fuzz.cpp
+++ TcpProxyFuzzer/Fuzz.cpp
@@ -1,27 +1,57 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <conio.h>
 #include <memory.h>
 #include <sal.h>
 #include <memory>
-#include <intrin.h>
 #include <cstdio>
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <intrin.h>
 
 constexpr size_t MIN_BUFF_LEN = 8;
 
-static unsigned int GetRand() noexcept {
+std::vector<std::string> naughtyStrings;
+bool naughtyStringsLoadAttempted = false;
+
+// using the CPU for rand numbers
+static unsigned int GetRand() noexcept { 
 	unsigned int rndVal{};
-	while (_rdrand32_step(&rndVal) == 0);  
+	while (_rdrand32_step(&rndVal) == 0);
 	return rndVal;
 }
 
+// this is called multiple times, usually per block of data
 bool Fuzz(_Inout_updates_bytes_(*pLen)	char* pBuf,
 		  _Inout_						size_t* pLen,
-		  _In_							unsigned int fuzzaggr) noexcept {
+		  _In_							unsigned int fuzzaggr) {
+
+	// on first call, load the naughty strings file
+	// the 'attempted' flag is to prevent trying to load the file
+	// if the file does not exist or there's a load error
+	if (naughtyStrings.empty() && !naughtyStringsLoadAttempted) {
+		naughtyStringsLoadAttempted = true;
+
+		std::ifstream inputFile("naughty.txt", std::ios::in | std::ios::binary);
+		if (inputFile.is_open()) {
+			std::string line;
+			while (std::getline(inputFile, line)) {
+				// Check if the line is non-empty and does not start with #
+				if (!line.empty() && line[0] != '#') {
+					naughtyStrings.push_back(line);
+				}
+			}
+		}
+
+		inputFile.close();
+	}
 
 	// don't fuzz everything
-	if ((GetRand() % 100) > fuzzaggr)
+	if ((GetRand() % 100) > fuzzaggr) {
+		printf("Non");
 		return false;
+	}
 
 	// check for nulls
 	if (pBuf == nullptr || pLen == nullptr) {
@@ -48,23 +78,23 @@ bool Fuzz(_Inout_updates_bytes_(*pLen)	char* pBuf,
 	}
 
 	// how many loops through the fuzzer?
-	// most of the, 10%, keep it at one iteration
+	// most of the time, 10%, keep it at one iteration
 	const unsigned int iterations = GetRand() % 10 == 7
 		? 1
 		: 1 + GetRand() % 10;
 
-	for (unsigned int i = 0; i < iterations; i++) {
+	// This is where the work is done
+	for (size_t i = 0; i < iterations; i++) {
 
 		const unsigned int skip = GetRand() % 10 > 7 ? 1 + GetRand() % 10 : 1;
 		const unsigned int whichMutation = GetRand() % 8;
 		unsigned int j = 0;
 
-		printf("%u", whichMutation);
-
 		switch (whichMutation) {
 		// set the range to a random byte
 		case 0:
 		{
+			printf("Byt");
 			const char byte = GetRand() % 256;
 			for (j = start; j < end; j += skip) {
 				pBuf[j] = byte;
@@ -75,6 +105,7 @@ bool Fuzz(_Inout_updates_bytes_(*pLen)	char* pBuf,
 		// write random bytes to the range
 		case 1:
 		{
+			printf("Rnd");
 			for (j = start; j < end; j += skip) {
 				pBuf[j] = GetRand() % 256;
 			}
@@ -83,21 +114,24 @@ bool Fuzz(_Inout_updates_bytes_(*pLen)	char* pBuf,
 
 		// set upper bit
 		case 2:
+			printf("Sup");
 			for (j = start; j < end; j += skip) {
 				pBuf[j] |= 0x80;
 			}
 			break;
 
-			// reset upper bit
+		// reset upper bit
 		case 3:
+			printf("Rup");
 			for (j = start; j < end; j += skip) {
 				pBuf[j] &= 0x7F;
 			}
 			break;
 
-			// set the first zero-byte found to non-zero
+		// set the first zero-byte found to non-zero
 		case 4:
 		{
+			printf("Zer");
 			for (j = start; j < end; j += skip) {
 				if (pBuf[j] == 0) {
 					pBuf[j] = GetRand() % 256;
@@ -107,12 +141,13 @@ bool Fuzz(_Inout_updates_bytes_(*pLen)	char* pBuf,
 		}
 		break;
 
-		// insert interesting numbers
+		// insert interesting edge-case numbers, often 2^n +/- 1
 		case 5:
 		{
+			printf("Num");
 			const int interestingNum[] = { 0,1,7,8,9,15,16,17,31,32,33,63,64,65,127,128,129,191,192,193,223,224,225,239,240,241,247,248,249,253,254,255 };
 			for (j = start; j < end; j += skip) {
-				pBuf[j] = (char)interestingNum[GetRand() % _countof(interestingNum)];
+				pBuf[j] = static_cast<char>(interestingNum[GetRand() % _countof(interestingNum)]);
 			}
 		}
 		break;
@@ -120,7 +155,8 @@ bool Fuzz(_Inout_updates_bytes_(*pLen)	char* pBuf,
 		// interesting characters
 		case 6:
 		{
-			const char interestingChar[] = { ':', ';', '<', '>', '\\', '/', '.', '%', '-.','#', '@', '?', '+', '=', '|'};
+			printf("Chr");
+			const char interestingChar[] = { '~', '!', ':', ';', '<', '>', '\\', '/', '.', '%', '-','#', '@', '?', '+', '=', '|', '\n', '\r', '\t', '*', '[', ']', '{', '}', '.'};
 			for (j = start; j < end; j += skip) {
 				pBuf[j] = interestingChar[GetRand() % _countof(interestingChar)];
 			}
@@ -129,12 +165,59 @@ bool Fuzz(_Inout_updates_bytes_(*pLen)	char* pBuf,
 
 		// truncate
 		case 7:
+			printf("Trn");
 			*pLen = end;
 			break;
+
+		// overlong UTF-8 encodings
+		case 8: 
+		{
+			printf("Utf");
+			std::vector<unsigned char> overlong;
+			const int choice = GetRand() % 3;
+			const char base_char = GetRand() % 256;
+
+			// just to make sure we don't run off the end of the buffer
+			// max encoding len in 4, so this is a little more conservative
+			// TODO: might use int overflow checks here instead
+			if (end-start < MIN_BUFF_LEN/2)
+				start = end - MIN_BUFF_LEN/2;
+
+			switch (choice) {
+
+				case 0:
+					// 2-byte overlong encoding
+					overlong.push_back(0b11000000 | (base_char >> 6));
+					overlong.push_back(0b10000000 | (base_char & 0b00111111));
+					break;
+
+				case 1:
+					// 3-byte overlong encoding
+					overlong.push_back(0b11100000);
+					overlong.push_back(0b10000000 | (base_char >> 6));
+					overlong.push_back(0b10000000 | (base_char & 0b00111111));
+					break;
+
+				case 2:
+					// 4-byte overlong encoding
+					overlong.push_back(0b11110000);
+					overlong.push_back(0b10000000 | (base_char >> 6));
+					overlong.push_back(0b10000000 | (base_char & 0b00111111));
+					overlong.push_back(0b10000000);
+					break;
+
+				default:
+					break;
+				}
+
+				for (j = start; j < start + overlong.size(); j += skip) 
+					pBuf[j] = overlong.at(j - start);
+		}
 
 		default:
 			break;
 		}
+
 	}
 
 	return true;
--- TcpProxyFuzzer/TcpProxyFuzzer.cpp
+++ TcpProxyFuzzer/TcpProxyFuzzer.cpp
@@ -1,6 +1,7 @@
 // A TCP bi-directional proxy that can act as a fuzzer
-// Michael Howard (mikehow@microsof.com)
+// Michael Howard (mikehow@microsoft.com)
 // Azure Database Security
+// Last updated 11/16/2023
 
 #define  _WINSOCK_DEPRECATED_NO_WARNINGS 1
 
@@ -9,10 +10,18 @@
 #include <ws2tcpip.h>
 #include <process.h>  
 #include <windows.h>
+#include <time.h>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <cctype>
+#include <chrono>
+#include <sstream>
+#include <iomanip>
 
 #pragma comment(lib, "ws2_32.lib")
 
-#define BUFFER_SIZE 4096
+constexpr size_t BUFFER_SIZE = 4096;
 
 // Passes important info to the socket threads 
 // because thread APIs only support void* for args
@@ -22,22 +31,19 @@ typedef struct {
     int    sock_dir;    // this is the ACTUAL direction of the sockets, client->server (0), server->client (1)
     int    fuzz_dir;    // this is the requested fuzzing direction, c, s, b, n
     size_t fuzz_aggr;   // fuzzing aggressiveness
-    int    delay;
+    int    delay;	    // delay in msec before fuzzing starts, UNUSED
 } ConnectionData;
 
-void 
-    forward_data(_In_ ConnectionData*);
-
-unsigned __stdcall 
-    forward_thread(_In_  void*);
-
-bool 
-    Fuzz(_Inout_updates_bytes_(*pLen)	char* pBuf,
+std::string getCurrentTimeAsString();
+void forward_data(_In_ const ConnectionData*);
+unsigned __stdcall forward_thread(_In_  void*);
+bool Fuzz(_Inout_updates_bytes_(*pLen)	char* pBuf,
          _Inout_						size_t* pLen,
          _In_					        unsigned int fuzzaggr);
 
 int main(int argc, char* argv[]) {
 
+    srand(time(NULL));
     WSADATA wsaData{};
 
     if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
@@ -46,26 +52,33 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
+    // you must pass in all 6 args
+    // TODO: Replace with real arg parsing!
     if (argv==nullptr || argc != 7) {
 
         fprintf(stdout, "Usage: TcpProxyFuzzer <listen_port> <forward_ip> <forward_port> <start_delay> <aggressiveness> <fuzz_direction>\n");
         fprintf(stdout, "Where:\n\tlisten_port is the proxy listening port. Eg; 8088\n");
         fprintf(stdout, "\tforward_port is the port to proxy requests to. Eg; 80\n");
         fprintf(stdout, "\tforward_ip is the host to forward resuests to. Eg; 192.168.1.77\n");
-        fprintf(stdout, "\tstart_delay is how long to wait before fuzzing data in msec. Eg; 100 [Currently not implemented]\n");
+        fprintf(stdout, "\tstart_delay is how long to wait before fuzzing data in msec. Eg; 100 [Currently ignored and not implemented]\n");
         fprintf(stdout, "\taggressiveness is how agressive the fuzzing should be as a percentage between 0-100. Eg; 7\n");
         fprintf(stdout, "\tfuzz_direction determines whether to fuzz from client->server (s), server->client (c), none (n) or both (b). Eg; s\n");
 
         return 1;
     }
 
-    int             listen_port     = atoi(argv[1]);
-    char*           forward_ip      = argv[2];
-    int             forward_port    = atoi(argv[3]);
-    int             startdelay      = atoi(argv[4]);    // currently unused
-    unsigned int    aggressiveness  = atoi(argv[5]);
-    char            direction       = tolower(argv[6][0]);
+    // move argv into a vector for easier parsing
+    std::vector<std::string> args(argv, argv + argc);
 
+    // parse out cmd-line args
+    const int listen_port             = std::stoi(args.at(1));
+    const std::string forward_ip      = args.at(2);
+    const int forward_port            = std::stoi(args.at(3));
+    const int startdelay              = std::stoi(args.at(4)); // currently unused
+    const unsigned int aggressiveness = std::stoi(args.at(5));
+    const char direction              = std::tolower(args.at(6).at(0));
+
+    // basic error checking
     if (listen_port <= 0 || listen_port >= 65535 ||
         aggressiveness < 0 || aggressiveness > 100 ||
         (direction != 'c' && direction != 's' && direction != 'n' && direction != 'b')) {
@@ -87,7 +100,7 @@ int main(int argc, char* argv[]) {
     server_addr.sin_addr.s_addr = INADDR_ANY;
     server_addr.sin_port = htons(listen_port);
 
-    if (bind(server_sock, (SOCKADDR*)&server_addr, sizeof(server_addr)) == SOCKET_ERROR) {
+    if (bind(server_sock, reinterpret_cast<SOCKADDR*>(&server_addr), sizeof(server_addr)) == SOCKET_ERROR) {
         fprintf(stderr, "Bind failed. Error: %d\n", WSAGetLastError());
         closesocket(server_sock);
         WSACleanup();
@@ -101,7 +114,8 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    fprintf(stdout, "Proxying from port %d -> %s:%d\n", listen_port, forward_ip, forward_port);
+    fprintf(stdout, "TcpProxyFuzzer v1.03\n");
+    fprintf(stdout, "Proxying from port %d -> %s:%d\n", listen_port, forward_ip.c_str(), forward_port);
 
     while (1) {
         SOCKET client_sock = accept(server_sock, NULL, NULL);
@@ -120,10 +134,10 @@ int main(int argc, char* argv[]) {
         SOCKADDR_IN target_addr;
         memset(&target_addr, 0, sizeof(target_addr));
         target_addr.sin_family = AF_INET;
-        inet_pton(AF_INET, forward_ip, &target_addr.sin_addr);
+        inet_pton(AF_INET, forward_ip.c_str(), &target_addr.sin_addr);
         target_addr.sin_port = htons(forward_port);
 
-        if (connect(target_sock, (SOCKADDR*)&target_addr, sizeof(target_addr)) == SOCKET_ERROR) {
+        if (connect(target_sock, reinterpret_cast<SOCKADDR*>(&target_addr), sizeof(target_addr)) == SOCKET_ERROR) {
             fprintf(stderr, "Connect to target failed. Error: %d\n", WSAGetLastError());
             closesocket(target_sock);
             closesocket(client_sock);
@@ -146,23 +160,28 @@ int main(int argc, char* argv[]) {
 
 // this func handles both server->client and client->server
 unsigned __stdcall forward_thread(_In_ void* data) {
-    ConnectionData* connData = (ConnectionData*)data;
+    const ConnectionData* connData = static_cast<const ConnectionData*>(data);
     forward_data(connData);
 
     return 0;
 }
 
-void forward_data(_In_ ConnectionData *connData) {
+void forward_data(_In_ const ConnectionData *connData) {
     char buffer[BUFFER_SIZE]{};
     int bytes_received{};
     bool bFuzz = false;
 
     // fuzzing only happens in some instances
-    if ((connData->fuzz_dir == 'b') || 
-        (connData->sock_dir == 1 && connData->fuzz_dir == 'c') || (connData->sock_dir == 0 && connData->fuzz_dir == 's'))
+    if ((connData->fuzz_dir == 'b') 
+        || 
+        (connData->sock_dir == 1 && connData->fuzz_dir == 'c') 
+        || 
+        (connData->sock_dir == 0 && connData->fuzz_dir == 's'))
         bFuzz = true;
 
-    fprintf(stderr, "\n%c %d %c\t", connData->fuzz_dir, connData->sock_dir, bFuzz ? 'Y' : 'N');
+
+    if (bFuzz)
+        fprintf(stderr, "%s\t", getCurrentTimeAsString().c_str());
 
     // the recv() can be from the client or the server, this code is called on one of two threads
     while ((bytes_received = recv(connData->src_sock, buffer, BUFFER_SIZE, 0)) > 0) {
@@ -178,4 +197,21 @@ void forward_data(_In_ ConnectionData *connData) {
     // Clean up the sockets once we're done forwarding
     closesocket(connData->src_sock);
     closesocket(connData->dst_sock);
+
+    if (bFuzz)
+        fprintf(stderr, "\n");
 }
+
+#pragma warning (push)
+#pragma warning(disable : 4996)
+std::string getCurrentTimeAsString() {
+    auto currentTime = std::chrono::system_clock::now();
+    std::time_t currentTime_t = std::chrono::system_clock::to_time_t(currentTime);
+    std::tm* currentTime_tm = std::localtime(&currentTime_t);
+
+    std::ostringstream oss;
+    oss << std::put_time(currentTime_tm, "%H:%M:%S");
+
+    return oss.str();
+}
+#pragma warning (pop)
